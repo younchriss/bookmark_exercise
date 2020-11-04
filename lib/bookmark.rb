9e71011407665bb9ef39d21bdd1f36fa2a630f56
Christian Naykene <younchriss@live.co.uk>
2020-11-04 11:48:12 +0000

    Set up connection between Postgresql and Ruby

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,5 +1,10 @@
+require 'pg'
+
 class Bookmark
     def list
-        @bookmarks = ["https://www.google.com"]
+        connection = PG.connect(dbname: 'bookmark_manager')
+        result = connection.exec('SELECT * FROM bookmarks;')
+        result.map { |bookmark| bookmark['url'] }
+        #@bookmarks = ["https://www.google.com"]
     end
 end
